b006d846d65752a830b5396ba447faea426c2ca7
Audun Torp <audun@zedge.net>
2018-09-25 22:14:28 +0200

    Increase hidden layer size

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
+iterations = 1000
 learning_rate = 0.1
-hidden_nodes = 2
+hidden_nodes = 20
 output_nodes = 1
