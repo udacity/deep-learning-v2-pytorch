e52ca8e23645e486ab243b65344bf5b575b6a9da
Brian Smith <crazycabo350@gmail.com>
2020-10-27 17:54:55 +0000

    Upload model to AWS Sagemaker and get response index

--- project-dog-classification/code/inference.py
+++ project-dog-classification/code/inference.py
@@ -15,28 +15,15 @@ logger.setLevel(logging.DEBUG)
 
 def model_fn(model_dir):
     
+    logger.info('model_fn')
+    
     with torch.neo.config(model_dir=model_dir, neo_runtime=True):
         
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
         # The compiled model is saved as "compiled.pt"
-        model = torch.jit.load(os.path.join(model_dir, 'compiled.pt'))
+        model = torch.jit.load(os.path.join(model_dir, 'model.pth'))
         model = model.to(device)
-
-        # We recommend that you run warm-up inference during model load
-        sample_input_path = os.path.join(model_dir, 'sample_input.pkl')
-        
-        with open(sample_input_path, 'rb') as input_file:
-            model_input = pickle.load(input_file)
-        if torch.is_tensor(model_input):
-            model_input = model_input.to(device)
-            model(model_input)
-        elif isinstance(model_input, tuple):
-            model_input = (inp.to(device)
-                           for inp in model_input if torch.is_tensor(inp))
-            model(*model_input)
-        else:
-            print("Only supports a torch tensor or a tuple of torch tensors")
 
         return model
 
@@ -51,14 +38,14 @@ def transform_fn(model, payload, request_content_type, response_content_type):
     # preprocess
     decoded = Image.open(io.BytesIO(payload))
     preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(
-            mean=[
-                0.485, 0.456, 0.406], std=[
-                0.229, 0.224, 0.225]),
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5]
+        )
     ])
+    
     normalized = preprocess(decoded)
     batchified = normalized.unsqueeze(0)
     
